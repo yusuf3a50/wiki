ab4294c075a9ff7b3cf5fcdd84a4d0cfd041a5a6
Yusuf <ybyphone@disroot.org>
2024-12-06 12:49:51 +0000

    add update command and view traffic command to bash_aliases file

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -2,7 +2,6 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"';
 alias egrep='egrep --color=auto';
 alias fgrep='fgrep --color=auto';
-alias gitYusuf='git config user.name yusuf3a50 && git config user.email ybyphone@disroot.org';
 alias grep='grep --color=auto';
 alias l='ls -CF';
 alias la='ls -A';
@@ -14,3 +13,5 @@ alias up='sudo apt update && sudo apt upgrade -y';
 alias ipt='sudo iptables -L -v';
 alias fw='sudo ufw status verbose';
 alias lsofpin='watch "sudo lsof -P -i -n"';
+alias traf='nload -u K wlan0';
+alias up='sudo apt update && sudo apt upgrade -y';
